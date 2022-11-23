c971bb9939422352fe887c33930c94461d2d431c
Sabuto <robe_dunne@hotmail.com>
2022-11-23 20:45:54 +0000

    updated error response

--- src/Exceptions/LinodeApiException.php
+++ src/Exceptions/LinodeApiException.php
@@ -2,11 +2,13 @@
 
 namespace Sabuto\LaravelLinode\Exceptions;
 
+use Illuminate\Support\Arr;
+
 class LinodeApiException extends \Exception
 {
     public static function make(array $errors): self
     {
-        $error = $errors[0]["reason"];
+        $error = Arr::get($errors, 'errors.reason');
         return new self($error);
     }
 }

87fe0f97b084d66c41fa53804874fb97289bc455
Sabuto <robe_dunne@hotmail.com>
2022-11-27 13:10:03 +0000

    start implementing oauth

--- config/linode.php
+++ config/linode.php
@@ -3,4 +3,6 @@
 // config for Sabuto/LaravelLinode
 return [
     'key' => env('LINODE_KEY'),
+    'client_id' => env('LINODE_CLIENT'),
+    'client_secret' => env('LINODE_SECRET')
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('linode')->group(function () {
-    Route::get('/callback', function () {
-        dd('test');
+    Route::post('/callback', function (Request $request) {
+        dd($request);
+    });
+
+    Route::get('/authorise', function (){
+        Http::asForm()->post('https://login.linode.com/oauth/token',[
+            'client_id' => config('linode.client_id'),
+            'client_secret' => config('linode.client_secret')
+        ]);
     });
 });

985be21760d65a515ac2b344e7794a57fa463a76
Sabuto <robe_dunne@hotmail.com>
2022-11-27 15:49:37 +0000

    start implementing oauth

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,11 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('linode')->group(function () {
-    Route::post('/callback', function (Request $request) {
-        dd($request);
+    Route::get('/callback', function (Request $request) {
+        dd($request, $request->code);
     });
 
-    Route::get('/authorise', function (){
+    Route::get('/authorise', function () {
         $c = config('linode.client_id');
         $r = 'code';
         $scopes = 'linodes:read_write';

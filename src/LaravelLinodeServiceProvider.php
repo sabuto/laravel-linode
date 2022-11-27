cbee69da0897489b5864a9ca4ce87c5a9b4cd7ad
Sabuto <robe_dunne@hotmail.com>
2022-11-27 12:46:27 +0000

    add routeso

--- src/LaravelLinodeServiceProvider.php
+++ src/LaravelLinodeServiceProvider.php
@@ -18,7 +18,7 @@ class LaravelLinodeServiceProvider extends PackageServiceProvider
         $package
             ->name('laravel-linode')
             ->hasConfigFile()
-            ->hasRoutes(__DIR__ . '/../routes/web.php')
+            ->hasRoutes(__DIR__ . '../routes/web.php')
             ->hasCommand(LaravelLinodeCommand::class);
     }
 }

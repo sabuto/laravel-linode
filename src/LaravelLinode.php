3797305898f9e92a9d33d505923580674d7148c4
Sabuto <robe_dunne@hotmail.com>
2022-11-28 20:10:24 +0000

    updated for Facade

--- src/LaravelLinode.php
+++ src/LaravelLinode.php
@@ -6,13 +6,18 @@ use Sabuto\LaravelLinode\Api\Images;
 use Sabuto\LaravelLinode\Api\LinodeInstances;
 use Sabuto\LaravelLinode\Api\LinodeTypes;
 use Sabuto\LaravelLinode\Api\Regions;
+use Sabuto\LaravelLinode\Models\Linode;
 
 class LaravelLinode
 {
     use LinodeTypes, LinodeInstances, Regions, Images;
 
-    public function getKey(): string
+    public function __construct(protected Linode $linode)
     {
-        return config('linode.key');
+    }
+
+    public function delete($id)
+    {
+        $this->linode->deleteLinode($id);
     }
 }
